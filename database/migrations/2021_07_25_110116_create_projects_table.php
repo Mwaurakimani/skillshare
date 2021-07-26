44748ec6ae1fdccec084cdfa94c0434a771812b9
MwauraKimani <kimmwaus@gmail.com>
2021-07-26 10:53:34 +0300

    add unversioned files

---
+++ database/migrations/2021_07_25_110116_create_projects_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('complexity');
+            $table->integer('period');
+            $table->decimal('price',12,2);
+            $table->integer('rating');
+            $table->text('Description');
+            $table->bigInteger('user_id');
+            $table->string('image');
+            $table->boolean('complete')->default(false);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
