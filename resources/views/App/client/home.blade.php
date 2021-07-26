35497266b4e006ef104fbf4db18793dbb33a7349
delick123 <delickmecha@gmail.com>
2021-07-26 16:44:49 +0300

    TY

--- resources/views/App/client/home.blade.php
+++ resources/views/App/client/home.blade.php
@@ -1,35 +1,6 @@
 @extends('dashboard')
 
 @section('content')
-<<<<<<< HEAD
-    <div class="body-section">
-        <x-top-bar></x-top-bar>
-        <div class="content-body">
-            <div class="panel-heading">
-                <div class="btn-sect">
-                    <button type="submit" form="User_form">Update</button>
-                </div>
-                <div class="btn-sect">
-                    <button id="btn_delete_user">Delete</button>
-                </div>
-            </div>
-            @if(Session::has('message'))
-                <p  class="alert success-Update {{ Session::get('alert-class', 'alert-info') }}">{{ Session::get('message') }}</p>
-            @endif
-            @if ($errors->any())
-                <div class="alert alert-danger">
-                    <ul>
-                        @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-            <form id="User_form" class="User-form" action="/User/{{ Auth::User()->id }}" method="POST">
-                @method('PUT')
-                @csrf
-
-=======
     <div class="navigation-section">
         <div class="logo-section">
             <img src=" {{asset( 'storage/logo.png' )}} " alt="">
@@ -84,26 +55,11 @@
                 </div>
             </div>
             <form class="User-form" action="">
->>>>>>> 4bd199ac2787eaadf46a1e77d0a8787a3f6148e2
                 <div class="section-1">
                     <h4>User Form</h4>
                     <div class="field-area">
                         <div class="form-group">
                             <label for="exampleInputEmail1">UserName</label>
-<<<<<<< HEAD
-                            <input type="text" class="form-control" name="name"  aria-describedby="emailHelp"  value="{{ Auth::User()->name }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Email</label>
-                            <input type="email" class="form-control" name="email" aria-describedby="emailHelp"   value="{{ Auth::User()->email }}">
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Preferred Payment Method </label>
-                            <select name="paymentMethode" id="">
-                                <option value="Mpesa" {{ Auth::User()->visbility == 'Mpesa' ? 'selected="selected"' : "" }}>Mpesa</option>
-                                <option value="Bank" {{ Auth::User()->visbility == 'Bank' ? 'selected="selected"' : "" }} >Bank</option>
-                            </select>
-=======
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  >
                         </div>
                         <div class="form-group">
@@ -117,7 +73,6 @@
                         <div class="form-group">
                             <label for="exampleInputEmail1">Preferred Payment Method </label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  >
->>>>>>> 4bd199ac2787eaadf46a1e77d0a8787a3f6148e2
                         </div>
                     </div>
                 </div>
@@ -125,39 +80,16 @@
                     <h4>User Form</h4>
                     <div class="field-area">
                         <div class="form-group">
-<<<<<<< HEAD
-                            <label for="exampleInputEmail1">Visibilit</label>
-                            <select name="visibility" id="">
-                                <option value="true" {{ Auth::User()->visibility == 1 ? 'selected="selected"' : "" }}>true</option>
-                                <option value="false" {{ Auth::User()->visibility == 0 ? 'selected="selected"' : "" }} >false</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Status</label>
-                            <select name="status" id="">
-                                <option value="Active" {{ Auth::User()->status == 'Active' ? 'selected="selected"' : "" }}>Active</option>
-                                <option value="Inactive" {{ Auth::User()->status == 'Inactive' ? 'selected="selected"' : "" }} >Inactive</option>
-                            </select>
-                        </div>
-                        <div class="form-group">
-                            <label for="exampleInputEmail1">Status</label>
-                            <input type="text" name="accountType" value="{{ Auth::User()->role ? Auth::User()->role : "N/A"  }}" disabled>
-=======
                             <label for="exampleInputEmail1">Visibility</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  >
                         </div>
                         <div class="form-group">
                             <label for="exampleInputEmail1">Status</label>
                             <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp"  >
->>>>>>> 4bd199ac2787eaadf46a1e77d0a8787a3f6148e2
                         </div>
                     </div>
                 </div>
             </form>
         </div>
     </div>
-<<<<<<< HEAD
-    <script src=" {{ asset("js/App/user.js") }} " ></script>
-=======
->>>>>>> 4bd199ac2787eaadf46a1e77d0a8787a3f6148e2
 @endsection
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,21 +1,5 @@
-const mix = require('laravel-mix');
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
- | file for the application as well as bundling up all the JS files.
- |
- */
 
 mix.js('resources/js/app.js', 'public/js')
-<<<<<<< HEAD
-    .js('resources/js/App/user', 'public/js/App/user.js')
-=======
->>>>>>> 4bd199ac2787eaadf46a1e77d0a8787a3f6148e2
     .js('resources/js/res/live.js', 'public/js/res/live.js')
     .sass('resources/scss/app.scss', 'public/css/Assets/app.css')
     .postCss('resources/css/app.css', 'public/css', [
