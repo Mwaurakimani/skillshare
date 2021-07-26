ecbedc4a4159ea9e35fcd25103263d0134d01d15
delick123 <delickmecha@gmail.com>
2021-07-26 16:13:57 +0300

    ty

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -23,9 +23,6 @@ class CreateUsersTable extends Migration
             $table->string('status')->default('active');
             $table->string('role')->default('client');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
             $table->rememberToken();
             $table->timestamps();
         });
